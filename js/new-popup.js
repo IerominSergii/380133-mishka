6cfd405c6e0bec101121175d86231b8bde05463c
IerominSergii <iconboooook@gmail.com>
2017-10-20 11:08:04 +0300

    Реализуй работу с меню в мобильной версии с js

--- js/new-popup.js
+++ js/new-popup.js
@@ -1,34 +1,66 @@
 'use strict';
 
-var popup = document.querySelector('.basket-popup');
-var overlay = document.querySelector('.popup-overlay');
-var basketMenu = document.querySelector('.user-block__basket-link');
-var orderButton = document.querySelector('.goods-of-week__order');
-var unitBasket = document.querySelector('.unit__basket');
+(function () {
+  // переменные
+  var popup = document.querySelector('.basket-popup');
+  var overlay = document.querySelector('.popup-overlay');
+  var basketMenu = document.querySelector('.user-block__basket-link');
+  var orderButton = document.querySelector('.goods-of-week__order');
+  var unitBaskets = document.querySelectorAll('.unit__basket');
 
-var openPopup = function (evt) {
-  evt.preventDefault();
-  popup.classList.add('basket-popup--open');
-  popup.classList.remove('basket-popup--close');
-  overlay.classList.add('popup-overlay--open');
-  overlay.classList.remove('popup-overlay--close');
-};
+  // функции
+  var openPopup = function (evt) {
+    evt.preventDefault();
+    popup.classList.add('basket-popup--open');
+    popup.classList.remove('basket-popup--close');
+    overlay.classList.add('popup-overlay--open');
+    overlay.classList.remove('popup-overlay--close');
 
-var closePopup = function (evt) {
-  evt.preventDefault();
-  popup.classList.add('basket-popup--close');
-  popup.classList.remove('basket-popup--open');
-  overlay.classList.add('popup-overlay--close');
-  overlay.classList.remove('popup-overlay--open');
-};
+    basketMenu.removeEventListener('click', openPopup);
+    overlay.addEventListener('click', closePopup);
 
-basketMenu.addEventListener('click', openPopup);
+    if (orderButton) {
+      orderButton.removeEventListener('click', openPopup);
+    }
 
-overlay.addEventListener('click', closePopup);
+    if (unitBaskets.length > 0) {
+      for (var i = 0; i < unitBaskets.length; i++) {
+        unitBaskets[i].removeEventListener('click', openPopup);
+      }
+    }
+  };
 
-orderButton.addEventListener('click', openPopup);
+  var closePopup = function (evt) {
+    evt.preventDefault();
+    popup.classList.add('basket-popup--close');
+    popup.classList.remove('basket-popup--open');
+    overlay.classList.add('popup-overlay--close');
+    overlay.classList.remove('popup-overlay--open');
 
-unitBasket.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  openPopup();
-});
+    basketMenu.addEventListener('click', openPopup);
+    overlay.removeEventListener('click', closePopup);
+
+    if (orderButton) {
+      orderButton.addEventListener('click', openPopup);
+    }
+
+    if (unitBaskets.length > 0) {
+      for (var i = 0; i < unitBaskets.length; i++) {
+        unitBaskets[i].addEventListener('click', openPopup);
+      }
+    }
+  };
+
+  // обработчики событий
+  basketMenu.addEventListener('click', openPopup);
+
+  if (orderButton) {
+    orderButton.addEventListener('click', openPopup);
+  }
+
+  if (unitBaskets.length > 0) {
+    for (var i = 0; i < unitBaskets.length; i++) {
+      unitBaskets[i].addEventListener('click', openPopup);
+    }
+  }
+})();
